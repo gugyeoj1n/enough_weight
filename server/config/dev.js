90ec43cdbe8ac89355cd6fc25d23f6e2bc392234
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-11-24 11:41:35 +0900

    [CHORE] 기본 모듈 추가

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://gugyeoj1n:woojin9821@enough-weight.9aegfjw.mongodb.net/?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
-const express = require("express");
-const app = express();
-const port = 5000;
+// express 서버
+const express = require("express")
+const app = express()
+const port = 7676
+
+// MongoDB 연결
+const mongoose = require("mongoose")
+const dbKey = require("./config/dev")
+
+mongoose.connect(dbKey.mongoURI, { dbName: "enough-weight" })
+    .then(() => {
+        console.log("MongoDB CONNECTED")
+    }).catch(err => {
+        console.log(err)
+    })
+
+app.get("/main", (req, res) => {
+    res.send("HI")
+})
 
 app.listen(port, () => {
-    console.log("${port} connected.");
+    console.log(`${port} connected.`);
 });
