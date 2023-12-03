ef59a30e2c8867508b186d7bf5f02fc34ea2d5e5
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-12-03 16:59:02 +0900

    [FEAT] 프론트 열일중 ...

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import './MainPage.css'
 
 function MainPage()
 {
@@ -10,8 +11,31 @@ function MainPage()
 
     return (
         <div>
-            이정욱 제쳤고 ㅋㅋ
-            <button onClick={ loginNavigate }/>
+            <div className="background">
+                <div className="topBar">
+                    이정욱 제쳤고 ㅋㅋ
+                    <div className="account">
+                        
+                    </div>
+                    <div className="search">
+                        <img className="searchIcon" src="images/search.svg"/>
+                        <input className="searchInput" maxLength={ 20 }/>
+                    </div>
+                </div>
+                <div className="main">
+                    오 이게되네 ㅋㅋ
+                    <div className="category">
+                        <img className="categoryIcon" src="images/menu.png"/>
+                    </div>
+                </div>
+                <div className="navigationBar">
+                    <img src="images/home.png" className="navigationItem"/>
+                    <img src="images/search.svg" className="navigationItem"/>
+                    <img src="images/person.svg" className="navigationItem"/>
+                    <img src="images/edit.svg" className="navigationItem"/>
+                    <img src="images/setting.svg" className="navigationItem"/>
+                </div>
+            </div>
         </div>
     );
 }
