12ce3a94d9165e385413a45cc7bcf6025035c43c
이정욱 <65989344+ukly@users.noreply.github.com>
2023-12-17 16:50:16 +0900

    [FIX] 주석 제거

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -4,7 +4,7 @@ const article = require('../models/Article');
 exports.showProfile = async (req, res, next) => {
     try {
         const nickname = req.user.nickname;
-        console.log(nickname);
+
         const userArticles = await article.find({ "author" : nickname }).exec();
 
         const userRoutines = await routine.find({ "author" : nickname }).exec();
